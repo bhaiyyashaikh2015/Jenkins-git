762b3e59af1b66ccad631a029ccd0caa9364ab3b
Bhaiyya Shaikh <bhaiyyashaikh2015@gmail.com>
2021-09-09 17:17:52 +0530

    added java code and readme.txt file

--- Jenkins_Git.java
+++ Jenkins_Git.java
@@ -5,7 +5,7 @@ public class Jenkins_Git
 		System.out.println("Integrating GIT with JENKINS");
 
 		System.out.println("performing build at the time of commit");
-
+// need to push also
 		for( int i=1; i<=5; i++)
 		{
 			System.out.println("Successfully build at the time of commit...This will prints 5 times");
