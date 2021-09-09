8289a4799407665c36f2bb6c9a39611b74f00c74
Bhaiyya Shaikh <bhaiyyashaikh2015@gmail.com>
2021-09-09 17:15:20 +0530

    My First commit

--- Jenkins_Git.java
+++ Jenkins_Git.java
@@ -3,6 +3,15 @@ public class Jenkins_Git
 	public static void main(String[] args) 
 	{
 		System.out.println("Integrating GIT with JENKINS");
+
+		System.out.println("performing build at the time of commit");
+
+		for( int i=1; i<=5; i++)
+		{
+			System.out.println("Successfully build at the time of commit...This will prints 5 times");
+		}
+
+
 		
 	}
 }
